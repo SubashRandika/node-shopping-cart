9898298a0fc5d7affc4e444729e4274592d6c8a4
Randika Rodrigo <subash.rodrigo@gmail.com>
2020-12-18 21:09:49 +0530

    Added ejs pug handlebars templating engines created shop template using pug language combine with express.

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
